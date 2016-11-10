42eaeaa9a671200c3c4a308e7447bb3a89003812
Danijel Pavić <danijelpavic21@gmail.com>
2016-11-10 21:13:56 +0100

    Rješeni su podrazumijevani argumenti.

--- PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
+++ PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
@@ -4,15 +4,17 @@ namespace Vsite.CSharp
 {
     public class PreopterećenjePorazumijevaniArg
     {
-        // TODO: Pokrenuti program i provjeriti ispise.
         public static int Zbroji(int x = 1, int y = 2, int z = 3) 
         {
             return x + y + z;
         }
 
-        // TODO: Preopteretiti funkciju Zbroji novom funkcijom public static int Zbroji(int x, int y) koja će vratiti zbroj argumenata. Ponovno pokrenuti programa te usporediti ispise.
+        public static int Zbroji(int x, int y)
+        {
+            return x + y;
+        }
 
-        
+
         static void Main(string[] args)
         {
             Console.WriteLine(Zbroji(5, 6, 7));
