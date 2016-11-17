96182308ec849c66f9b194b3c2a223c208889e02
Danijel Pavić <danijelpavic21@gmail.com>
2016-11-17 21:17:40 +0100

    Implementacija geometrijskog lika Trokut.

---
+++ CrtanjeLikova/GeometrijskiLikovi/Trokut.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrtanjeLikova.GeometrijskiLikovi
+{
+    class Trokut : GeometrijskiLik
+    {
+        public Trokut(float x1, float y1, float x2, ,float y2, float x3, float y3)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+        }
+
+        float x1;
+        float y1;
+        float x2;
+        float y2;
+        float x3;
+        float y3;
+
+        public override void Nacrtaj(Graphics g)
+        {
+            using (Pen pero = new Pen(Color.Green))
+            {
+                // g.DrawLine(pero, new PointF[] {new PointF(x1, y1)}, new PointF(x2, y2)}, new PointF(x3, y3)});
+            }
+        }
+
+        public override void Pomakni(float deltaX, float deltaY)
+        {
+
+        }
+
+        public override void Uvećaj(float faktor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
